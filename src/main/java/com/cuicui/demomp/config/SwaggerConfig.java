8efea7b40b12104fe470c8767135b4840fdd569a
Cuicui <yan_mw@163.com>
2020-06-08 14:21:49 +0800

    添加swagger相关演示

--- src/main/java/com/cuicui/demomp/config/SwaggerConfig.java
+++ src/main/java/com/cuicui/demomp/config/SwaggerConfig.java
@@ -36,7 +36,7 @@ public class SwaggerConfig {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("com.cuicui.demomp.*.controller")).paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.cuicui.demomp")).paths(PathSelectors.any())
                 .build().pathMapping("/")
                .enable(enable);
     }
--- src/main/java/com/cuicui/demomp/sys/controller/UserController.java
+++ src/main/java/com/cuicui/demomp/sys/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.cuicui.demomp.sys.controller;
 
 
+import com.cuicui.demomp.sys.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/sys/user")
+@Api(tags = "用户管理")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/hello")
+    @ApiOperation(value = "你好")
+    public String hello() {
+        return "ok";
+    }
+
 
 }
